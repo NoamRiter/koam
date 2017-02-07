8a33c5e161696d945e296524cabf8f90731b87c2
noam riter <noamriter@hotmail.com>
2017-02-07 14:44:12 +0100

    Init

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import{Component} from 'angular2/core';
+
+@Component({
+    selector:'my-app',
+    template: '<h1>noams app working</h1>'
+})
+
+export class AppComponent {}
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import 'core-js';
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app/app.component';
+
+bootstrap(AppComponent);
